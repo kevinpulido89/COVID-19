aa82f7c6553167cc7e7021dde70807d107460d4b
Kevin Pulido <kevinp8001@gmail.com>
2020-03-31 02:11:28 -0500

    Edit 'Daily_Growth.py'.
    Add main .xlsx file as dataframe

---
+++ Daily_Growth.py
@@ -0,0 +1,8 @@
+#COVID-19
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_excel('https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-2020-03-30.xlsx')
+
+print(df.head())
