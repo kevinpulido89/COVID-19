a68ed68fa49e79543c530f7dbe267e58f7893bf6
Kevin Pulido <kevinp8001@gmail.com>
2020-04-08 00:55:31 -0500

    Actualización del 8 abril

--- Daily_Growth.py
+++ Daily_Growth.py
@@ -62,7 +62,7 @@ def PlotPolly(model, independent_variable, dependent_variabble, N):
     plt.plot(independent_variable, dependent_variabble, '.', label='Casos confirmados')
     plt.plot(x_new, y_new, '-', label='Proyección Casos COL')
     plt.plot(N, int(New_case), color = 'r', marker = 'x', label = '#Casos Esperado')
-    plt.plot(N, 1579, color = 'g', marker = 'D', label = '#Casos Reales')
+    plt.plot(N, 1780, color = 'g', marker = 'D', label = '#Casos Reales')
     plt.title('Crecimiento de casos de COVID-19 en Colombia.')
     ax = plt.gca()
     ax.set_facecolor((0.892, 0.892, 0.892))
@@ -91,7 +91,7 @@ for i in range(len(acc)-window_size):
     acc_col = acc_col.append(pd.DataFrame(K).T)
 
 # Here we use a polynomial of the 5th order
-order = 6
+order = 5
 f = np.polyfit(yy, acc, order)
 # print('f: ',f)
 p = np.poly1d(f)
